3ff23bd733b3f909199c425a2bb1ca3b89c008a3
sobanarshad85 <sobanarshad85@gmail.com>
2023-03-17 14:33:33 +0500

    background Image at home screen added.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
+import {ImageBackground, StyleSheet} from 'react-native';
 import styled from 'styled-components/native';
 import Header from './Header';
 import Routines from './Routines';
@@ -10,14 +10,33 @@ const HomeContainer = styled.View`
   flex: 1;
 `;
 
+const BackgroundImage = styled(ImageBackground)`
+  flex: 1;
+`;
+
 function Home(): JSX.Element {
   return (
     <HomeContainer>
-      <Header />
-      <Routines />
-      <RoutineListing />
+      <BackgroundImage
+        source={require('../../assets/morning.png')}
+        resizeMethod="auto"
+        resizeMode="contain"
+        imageStyle={styles.imageStyle}>
+        <Header />
+        <Routines />
+        <RoutineListing />
+      </BackgroundImage>
     </HomeContainer>
   );
 }
 
 export default Home;
+
+const styles = StyleSheet.create({
+  imageStyle: {
+    height: 250,
+    width: 'auto',
+    marginTop: 350,
+    opacity: 0.25,
+  },
+});
